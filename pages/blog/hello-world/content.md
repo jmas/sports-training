a6d9aff907b42d8df61c46ed626f29186679ff8e
jmas <jmas.ukraine@gmail.com>
2022-01-21 16:59:45 +0200

    Flat structure

---
+++ pages/blog/hello-world/content.md
@@ -0,0 +1,6 @@
+---
+title: Hello world
+date: 2022-01-20 15:00
+---
+
+Hello world from Blog!
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+export const getContent = async () => {
+  return "";
+};
+
+export const getChildren = async () => {
+  return [{}];
+};
+
+export const getMeta = async () => {
+  return {
+    slug: "",
+    date: "2022-01-20 15:40",
+  };
+};
+
+export const getConfig = async () => {
+  return {
+    layout: "default",
+  };
+};
