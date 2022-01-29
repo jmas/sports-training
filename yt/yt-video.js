82274e7e5c42eff6b5af04d6bb0ab9d8cb3d0837
jmas <jmas.ukraine@gmail.com>
2022-01-29 20:03:20 +0200

    Update yt-video

--- yt/yt-video.js
+++ yt/yt-video.js
@@ -3,6 +3,7 @@ customElements.define(
   class extends HTMLElement {
     constructor() {
       super();
+      this._fetching = false;
     }
 
     static get observedAttributes() {
@@ -13,17 +14,56 @@ customElements.define(
       return this.getAttribute("video-id");
     }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (oldValue === newValue) {
-        return;
-      }
-      if (name === "video-id") {
-        this._fetchAndRender(this.videoId);
-      }
+    get title() {
+      return this.getAttribute("title");
     }
 
+    get channelPicture() {
+      return this.getAttribute("channel-picture");
+    }
+
+    connectedCallback() {
+      this._renderPlaceholder();
+      this._addIntersectionObserver();
+    }
+
+    _addIntersectionObserver = () => {
+      this._intersectionObserver = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            if (this.videoId && !this._fetching) {
+              this._fetchAndRender(this.videoId);
+            }
+          }
+        },
+        {
+          threshold: [0],
+        }
+      );
+      this._intersectionObserver.observe(this);
+    };
+
+    _removeIntersectionObserver = () => {
+      this._intersectionObserver.unobserve(this);
+      this._intersectionObserver = undefined;
+    };
+
     _fetchAndRender = (videoId) => {
-      this._fetch(videoId).then((data) => this._render(videoId, data));
+      this._fetching = true;
+      const override = (() => {
+        const override = {};
+        if (this.title) {
+          override.title = this.title;
+        }
+        return override;
+      })();
+      this._fetchOrGetFromCache(videoId).then((data) => {
+        this._render(videoId, {
+          ...data,
+          ...override,
+        });
+        this._fetching = false;
+      });
     };
 
     _fetch = (videoId) => {
@@ -32,17 +72,43 @@ customElements.define(
       ).then((response) => response.json());
     };
 
+    _fetchOrGetFromCache = (videoId) => {
+      const fromCache = localStorage[`yt_video_${videoId}_cache`];
+      if (fromCache) {
+        return Promise.resolve(JSON.parse(fromCache));
+      } else {
+        return this._fetch(videoId).then((data) => {
+          localStorage[`yt_video_${videoId}_cache`] = JSON.stringify(data);
+          return data;
+        });
+      }
+    };
+
+    _renderPlaceholder = () => {
+      this.innerHTML = `<span class="placeholder"></span>`;
+    };
+
     _render = (videoId, { title, thumbnail_url }) => {
       this.innerHTML = `
         <a
           href="https://www.youtube.com/watch?v=${videoId}"
           target="_blank"
         >
-          <img
-            src="${thumbnail_url}"
-            alt="${title}"
-          />
-          <span>${title}</span>
+          <span style="background-image: url(${thumbnail_url});">
+            <img
+              src="${thumbnail_url}"
+              loading="lazy"
+              alt="${title}"
+            />
+          </span>
+          <span>
+            ${
+              this.channelPicture
+                ? `<span style="background-image: url(${this.channelPicture});"></span>`
+                : `<span style="display: none;"></span>`
+            }
+            <span>${title}</span>
+          </span>
         </a>
       `;
     };
